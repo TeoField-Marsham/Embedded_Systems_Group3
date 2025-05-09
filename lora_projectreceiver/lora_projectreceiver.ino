690a0c6f7bfd54a3333323ec54a4bc035329bcd8
ThiTeiSan <167451264+ThiTeiSan@users.noreply.github.com>
2025-05-09 10:49:11 +0200

    Add files via upload

--- lora_projectreceiver/lora_projectreceiver.ino
+++ lora_projectreceiver/lora_projectreceiver.ino
@@ -1,7 +1,7 @@
 #include <SPI.h>
 #include <LoRa.h>
 
-// Pinos SPI/LoRa para a LilyGO T3 V1.6.1
+// Pins SPI/LoRa para a LilyGO T3 V1.6.1
 #define SCK 5
 #define MISO 19
 #define MOSI 27
@@ -9,34 +9,34 @@
 #define RST 14
 #define DIO0 26
 
-#define BAND 915E6  // Frequência LoRa: use 915E6 (Américas), 866E6 (Europa), ou 433E6 (Ásia)
+#define BAND 915E6  // Frequencies LoRa: use 915E6 (Americas), 866E6 (Europa), ou 433E6 (Asia)
 
 void setup() {
   Serial.begin(115200);
   while (!Serial);
 
-  // Inicializa a interface SPI
+  // SPI interface
   SPI.begin(SCK, MISO, MOSI, SS);
 
-  // Configura os pinos do LoRa
+  // LoRa pins
   LoRa.setPins(SS, RST, DIO0);
 
-  Serial.println("Inicializando LoRa Receiver...");
+  Serial.println("Initializing LoRa Receiver...");
   if (!LoRa.begin(BAND)) {
-    Serial.println("Falha ao iniciar o módulo LoRa!");
+    Serial.println("Fail to start LoRa!");
     while (1);
   }
 
-  Serial.println("LoRa iniciado com sucesso! Esperando pacotes...");
+  Serial.println("LoRa started! Waiting for data...");
 }
 
 void loop() {
-  // Verifica se chegou algum pacote
+  // Verify if there is a packet
   int packetSize = LoRa.parsePacket();
   if (packetSize) {
-    Serial.print("Pacote recebido: ");
+    Serial.print("Packet received: ");
 
-    // Lê o conteúdo do pacote
+    // Read packet
     while (LoRa.available()) {
       char c = LoRa.read();
       Serial.print(c);
